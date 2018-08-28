f76470275b4361e4fc3d1d023a8020a7fbdacb0e
tychejin1218 <tychejin@naver.com>
2018-08-28 17:24:46 +0900

    [게시판]설정 파일 변경(web.xml, *-context.xml)

---
+++ board/src/main/java/com/spring/board/SampleController.java
@@ -0,0 +1,17 @@
+package com.spring.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/sample")
+public class SampleController {
+
+	@RequestMapping(value = "/getSample")
+	public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {		
+		System.out.println("getSample");
+	}
+}
