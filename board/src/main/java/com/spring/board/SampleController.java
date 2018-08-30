a2af3a03a1cc8ce8deb641938b76f889b468e99f
tychejin1218 <tychejin@naver.com>
2018-08-30 16:56:31 +0900

    [게시판] MySQL 연동관련 내용

--- board/src/main/java/com/spring/board/SampleController.java
+++ board/src/main/java/com/spring/board/SampleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
 
 	@RequestMapping(value = "/getSample")
-	public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {		
+	public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {
 		System.out.println("getSample");
 	}
 }
