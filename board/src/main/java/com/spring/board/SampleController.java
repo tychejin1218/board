78c716ba2c3abca1a4c3466dee6e73470bddd9cc
tychejin1218 <tychejin@naver.com>
2018-08-31 18:06:38 +0900

    [게시판] log4j->logback으로 변경

--- board/src/main/java/com/spring/board/SampleController.java
+++ board/src/main/java/com/spring/board/SampleController.java
@@ -3,6 +3,8 @@ package com.spring.board;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/sample")
 public class SampleController {
 
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping(value = "/getSample")
 	public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {
-		System.out.println("getSample");
+		logger.info("================== getSample ==================");
 	}
 }
